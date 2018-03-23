2268a1b44d16a7b2d8d5458e9c1318730525789c
Suneet Bhatnagar <suneet.bhatnagar@gmail.com>
2018-03-23 10:49:50 +0000

    Added MOTD recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,14 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-#
+ if node[platform_family] == "rehl"
+     package="httpd"
+ elsif node[platform_family] == "debian"
+     package="apache2"
+end
+
 package 'apache2' do
-      package_name 'httpd'
+      package_name package
       action:install
 end
 
 service 'apache2' do
-      service_name 'httpd'
+      service_name  package
       action [:start , :enable]
 end
 
